a849535a76a600f925744b3ca97e2824e70f3334
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2019-10-17 16:25:44 +0800

    Modified Form in Official Receipt

--- EasyPOS/Reports/RepOfficialReceiptReportForm.Designer.cs
+++ EasyPOS/Reports/RepOfficialReceiptReportForm.Designer.cs
@@ -30,44 +30,32 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RepOfficialReceiptReportForm));
             this.printDocumentOfficialReceipt = new System.Drawing.Printing.PrintDocument();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // printDocumentOfficialReceipt
             // 
             this.printDocumentOfficialReceipt.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocumentOfficialReceipt_PrintPage);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(140, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Printing...";
-            // 
             // RepOfficialReceiptReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 133);
+            this.ClientSize = new System.Drawing.Size(482, 53);
             this.ControlBox = false;
-            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 10.2F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.MinimumSize = new System.Drawing.Size(100, 100);
+            this.MaximumSize = new System.Drawing.Size(500, 100);
+            this.MinimumSize = new System.Drawing.Size(500, 100);
             this.Name = "RepOfficialReceiptReportForm";
             this.Text = "Official Receipt";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Drawing.Printing.PrintDocument printDocumentOfficialReceipt;
-        private System.Windows.Forms.Label label1;
     }
 }
