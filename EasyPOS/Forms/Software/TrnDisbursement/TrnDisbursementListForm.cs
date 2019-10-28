abf0f6d26d671d9c3c2447d6963012771b4dcdc1
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2019-10-28 14:33:03 +0800

    Added Forms ready for CRUD

---
+++ EasyPOS/Forms/Software/TrnDisbursement/TrnDisbursementListForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyPOS.Forms.Software.TrnDisbursement
+{
+    public partial class TrnDisbursementListForm : Form
+    {
+        public TrnDisbursementListForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
