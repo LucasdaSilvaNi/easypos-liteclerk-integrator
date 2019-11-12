7be1c38d566f1b5e0c4677a4c5c9443eeffb6699
Oliver Enciso <oliverenciso07@gmail.com>
2019-11-12 14:56:01 +0800

    RepSalesReportTrnSalesEntity and RepSalesReportTrnCollectionEntity Added, RepSalesReportController Fixes

--- EasyPOS/Entities/RepSalesReportTrnSalesEntity.cs
+++ EasyPOS/Entities/RepSalesReportTrnSalesEntity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EasyPOS.Entities
 {
-    public class TrnSalesEntity
+    public class RepSalesReportTrnSalesEntity
     {
         public Int32 Id { get; set; }
         public Int32 PeriodId { get; set; }
