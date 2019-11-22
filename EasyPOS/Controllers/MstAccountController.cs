d049f3c4b03c1717b79b374d5a9b46e011a56b9a
Oliver Enciso <oliverenciso07@gmail.com>
2019-11-22 15:02:07 +0800

    API CRUD - MstAccount & MstPayType

--- EasyPOS/Controllers/MstAccountController.cs
+++ EasyPOS/Controllers/MstAccountController.cs
@@ -33,5 +33,90 @@ namespace EasyPOS.Controllers
 
             return accounts.OrderByDescending(d => d.Id).ToList();
         }
+
+        public List<String> ListType()
+        {
+            return new List<String>
+            {
+                "ASSET", "LIABILITY", "SALES", "EXPENSES"
+            };
+        }
+
+        public String[] AddAccount(Entities.MstAccountEntity objAccount)
+        {
+            try
+            {
+                Data.MstAccount addAccount = new Data.MstAccount()
+                {
+                    Code = objAccount.Code,
+                    Account = objAccount.Account,
+                    IsLocked = true,
+                    AccountType = objAccount.AccountType
+                };
+
+                db.MstAccounts.InsertOnSubmit(addAccount);
+                db.SubmitChanges();
+                return new String[] { "", "" };
+            }
+            catch (Exception e)
+            {
+                return new String[] { e.Message, "0" };
+            }
+        }
+
+        public String[] UpdateAccount(Entities.MstAccountEntity objAccount)
+        {
+            try
+            {
+                var currentAccount = from d in db.MstAccounts
+                                     where d.Id == objAccount.Id
+                                     select d;
+                if (currentAccount.Any())
+                {
+                    var updateAccount = currentAccount.FirstOrDefault();
+                    updateAccount.Code = objAccount.Code;
+                    updateAccount.Account = objAccount.Account;
+                    updateAccount.AccountType = objAccount.AccountType;
+                    db.SubmitChanges();
+
+                    return new String[] { "", "" };
+                }
+                else
+                {
+                    return new String[] { "Account not found!", "0" };
+                }
+            }
+            catch (Exception e)
+            {
+                return new String[] { e.Message, "0" };
+            }
+        }
+
+        public String[] DeleteAccount(int id)
+        {
+            try
+            {
+                var account = from d in db.MstAccounts
+                              where d.Id == id
+                              select d;
+                if (account.Any())
+                {
+                    var deleteAccount = account.FirstOrDefault();
+                    db.MstAccounts.DeleteOnSubmit(deleteAccount);
+                    db.SubmitChanges();
+
+                    return new string[] { "", "" };
+                }
+                else
+                {
+                    return new String[] { "Account not found!", "0" };
+                }
+            }
+            catch (Exception e)
+            {
+                return new String[] { e.Message, "0" };
+            }
+        }
+
     }
 }
--- EasyPOS/Controllers/MstPayTypeController.cs
+++ EasyPOS/Controllers/MstPayTypeController.cs
@@ -31,5 +31,89 @@ namespace EasyPOS.Controllers
 
             return payTypes.OrderBy(d => d.Id).ToList();
         }
+
+        public List<Entities.MstAccountEntity> DropDownListAccount()
+        {
+            var accounts = from d in db.MstAccounts
+                           select new Entities.MstAccountEntity
+                           {
+                               Id = d.Id,
+                               Account = d.Account
+                           };
+            return accounts.ToList();
+        }
+
+        public String[] AddPayType(Entities.MstPayTypeEntity objPayType)
+        {
+            try
+            {
+                Data.MstPayType addPayType = new Data.MstPayType()
+                {
+                    PayType = objPayType.PayType,
+                    AccountId = objPayType.AccountId
+                };
+                db.MstPayTypes.InsertOnSubmit(addPayType);
+                db.SubmitChanges();
+
+                return new string[] { "", "" };
+            }
+            catch (Exception e)
+            {
+                return new String[] { e.Message, "0" };
+            }
+        }
+
+        public String[] UpdatePayType(Entities.MstPayTypeEntity objPayType)
+        {
+            try
+            {
+                var currentPaytype = from d in db.MstPayTypes
+                                     where d.Id == objPayType.Id
+                                     select d;
+                if (currentPaytype.Any())
+                {
+                    var updatePayType = currentPaytype.FirstOrDefault();
+                    updatePayType.PayType = objPayType.PayType;
+                    updatePayType.AccountId = objPayType.AccountId;
+                    db.SubmitChanges();
+
+                    return new String[] { "", "" };
+                }
+                else
+                {
+                    return new String[] { "Pay Type not found!", "0" };
+                }
+            }
+            catch (Exception e)
+            {
+                return new string[] { e.Message, "0" };
+            }
+        }
+
+        public String[] DeletePayType(Int32 id)
+        {
+            try
+            {
+                var payType = from d in db.MstPayTypes
+                              where d.Id == id
+                              select d;
+                if (payType.Any())
+                {
+                    var deletePayType = payType.FirstOrDefault();
+                    db.MstPayTypes.DeleteOnSubmit(deletePayType);
+                    db.SubmitChanges();
+
+                    return new String[] { "", "" };
+                }
+                else
+                {
+                    return new String[] { "Pay Type not found", "0" };
+                }
+            }
+            catch (Exception e)
+            {
+                return new String[] { e.Message, "0" };
+            }
+        }
     }
 }
